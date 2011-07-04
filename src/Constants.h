2e9df50425d68642dbb464379783cae7f21c948a
Soup Can <spam _ criticaleffect net>
2011-07-04 17:00:01 +1000

    Added Constants.h

---
+++ src/Constants.h
@@ -0,0 +1,17 @@
+/*
+ *  constants.h
+ *  Jungle
+ *
+ *  Created by ollie on 4/07/11.
+ *  Copyright 2011 Oliver Marriott. All rights reserved.
+ *
+ */
+
+#ifndef _H_CONSTANTS
+#define _H_CONSTANTS
+
+const string kLAST_SEQUENCE_TOKEN = "####FINALSEQUENCE####";
+const int kNoUserAction = 0;
+const int kAttackerAction = 1;
+const int kVictimAction = 2;
+#endif
