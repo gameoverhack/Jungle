105ad27677f1bb1ca5f92957d6532c6389141978
Soup Can <spam _ criticaleffect net>
2011-07-15 04:42:51 +1000

    Added isMovieFaked display in DebugView (progress bar goes red on fake)

--- src/DebugView.cpp
+++ src/DebugView.cpp
@@ -47,19 +47,26 @@ void DebugView::update(){
 	ofDrawBitmapString(msg, 20, 20); // this gets rendered upside down for some reason ?*/
 	
 	// draw scene progression
-	float percent = (float)currentFrame/(float)(totalFrames);
-	float width = 300;
-	float height = 20;
-	ofSetColor(50, 50, 50);
-	ofRect(5, _viewHeight-height-10, width+2, height+2);
-	ofSetColor(200, 50, 50);
-	ofRect(5+1, _viewHeight-height-9, width*percent, height);
-
+	float progressionPercentage = (float)currentFrame/(float)(totalFrames);
+	float progressionWidth = 300;
+	float progressionHeight = 20;
+	ofSetColor(50, 50, 50, 220);
+	ofRect(5, _viewHeight-progressionHeight-10, progressionWidth+4, progressionHeight+2);
+	// valid movie file stuff
+	currentSequence->getIsMovieFaked() ? ofSetColor(255, 50, 50, 220) : ofSetColor(50, 220, 50, 220);
+	//	ofSetColor(200, 50, 50, 220);
+	ofRect(5+1, _viewHeight-progressionHeight-9, progressionWidth*progressionPercentage, progressionHeight);
+	
+	//ofRect(5+progressionWidth+2, _viewHeight-progressionHeight-10, 20, progressionHeight+2);
+	
+	
 	// give us info about progression
 	msg = currentScene->getName() + "::" + currentSequence->getName() + "::" + ofToString(currentFrame) + "/" + ofToString(totalFrames);
 	
 	ofSetColor(255,255,255);
-	ofDrawBitmapString(msg, 6, _viewHeight-height-4);
+	ofDrawBitmapString(msg, 6, _viewHeight-progressionHeight-4);
+	
+	
 
 	glPopMatrix();
 	_viewFBO.end();
