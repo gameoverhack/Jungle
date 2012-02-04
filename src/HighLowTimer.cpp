b79150be0c57a8c38b2b598b7a18bfb48da810bf
RKTJMP <spam _ criticaleffect net>
2012-02-05 04:25:38 +1100

    Adding missing Timer.cpp/.h and HighLowTimer.cpp/.h

---
+++ src/HighLowTimer.cpp
@@ -0,0 +1,205 @@
+/*
+ *  HighLowTimer.cpp
+ *
+ */
+
+#include "HighLowTimer.h"
+
+
+HighLowTimer::HighLowTimer(){
+//	setup(0, 1.0, 0, 0.0);
+}
+
+HighLowTimer::~HighLowTimer(){
+	ofRemoveListener(_highTimer.timerEvent, this, &HighLowTimer::highTimerEventCallback);
+	ofRemoveListener(_lowTimer.timerEvent, this, &HighLowTimer::lowTimerEventCallback);
+};
+
+
+// use this for limited high-low cycle count, use other for REPEAT or ONCE modes
+void HighLowTimer::setup(int highLengthInMilliseconds, float highValue,
+		   int lowLengthInMilliseconds, float lowValue,
+		   int cycleLimit,
+		   string timerName,
+		   int timerResolution){
+	// setup our timer, save some stuff
+	_highLength = highLengthInMilliseconds;
+	_highValue = highValue;
+	_lowLength = lowLengthInMilliseconds;
+	_lowValue = lowValue;
+	_mode = HIGHLOWTIMER_MODE_CYCLECOUNT;
+	_cycleLimit = cycleLimit;
+	_cycleCount = 0;
+	_timerResolution = timerResolution;
+	_name = timerName;
+
+	// setup the timers but don't start them here.
+	_highTimer.setTimeout(_highLength, TIMER_MODE_ONCE, _name + " High Timer");
+	ofAddListener(_highTimer.timerEvent, this, &HighLowTimer::highTimerEventCallback);
+
+	_lowTimer.setTimeout(_lowLength, TIMER_MODE_ONCE, _name + " Low Timer");
+	ofAddListener(_lowTimer.timerEvent, this, &HighLowTimer::lowTimerEventCallback);
+}
+
+
+// Use this setup for REPEAT or ONCE modes, use others for a limited high-low cycle count
+void HighLowTimer::setup(int highLengthInMilliseconds, float highValue,
+						 int lowLengthInMilliseconds, float lowValue,
+						 HighLowTimerMode mode,
+						 string timerName,
+						 int timerResolution){
+	setup(highLengthInMilliseconds, highValue, lowLengthInMilliseconds, lowValue, -1, timerName, timerResolution);
+	_mode = mode;
+}
+
+/* Public timer methods */
+
+bool HighLowTimer::start(){
+	return start(false); // do NOT suppress events
+}
+
+bool HighLowTimer::stop(){
+	return stop(false); // do NOT suppress events
+}
+
+bool HighLowTimer::restart(){
+	// start acts as restart
+	bool didStart = start(true); // suppress events
+	
+	if(didStart){
+		HighLowTimerEvent event = HighLowTimerEvent(_name, this, HIGHLOWTIMER_EVENT_START);
+		ofNotifyEvent(highLowTimerEvent, event, this);
+	}
+	
+	return didStart;
+}
+
+/* Private timer methods */ 
+bool HighLowTimer::start(bool suppressEvents){
+	// sanity check our values
+//	if(_highLength <= 0){
+//		cout << "HIGH LENGTH LESS THAN 0";
+//	}
+//	if(_lowLength <= 0){
+//		cout << "LOW LENGTH LESS THAN 0";
+//	}
+//	if(_timerResolution <= 0){
+//		cout << "TIMER RESOLUTION LESS THAN 0";
+//	}
+//	if(_cycleLimit <= 0){
+//		cout << "CYCLE LIMIT LESS THAN 0";
+//	}
+//	if(_mode != HIGHLOWTIMER_MODE_REPEAT){
+//		cout << "MODE IS NOT REPEAT";
+//	}
+//	if((_cycleLimit <= 0 && _mode != HIGHLOWTIMER_MODE_REPEAT)){
+//		cout << "NESTED IS TRUE";
+//	}
+//	if(_mode == HIGHLOWTIMER_MODE_UNKNOWN){
+//		cout << "MODE IS UNKNOWN";
+//	}
+//	
+	if(_highLength <= 0 || _lowLength <= 0 || _timerResolution <= 0 ||
+	   (_cycleLimit <= 0 && _mode != HIGHLOWTIMER_MODE_REPEAT) ||
+	   _mode == HIGHLOWTIMER_MODE_UNKNOWN){
+		return false;
+	}
+
+	
+	// stop lowTimer if if is running
+	_lowTimer.stop(); // TODO: suppress events here
+	
+	// start with highTimer
+	if(_highTimer.start()){
+		HighLowTimerEvent event = HighLowTimerEvent(_highTimer.getName(), _highValue, this, HIGHLOWTIMER_EVENT_HIGH_START);
+		ofNotifyEvent(highLowTimerEvent, event, this);
+		// dispatch high low timer started
+		event = HighLowTimerEvent(_name, this, HIGHLOWTIMER_EVENT_START);
+		ofNotifyEvent(highLowTimerEvent, event, this);
+		// start success
+		return true;
+	}
+	else{
+		// TODO: could notify start_fail but why bother, shoudl it be fail or start_fail? stop_fail also????
+		return false;
+	}
+}
+
+bool HighLowTimer::stop(bool suppressEvents){
+	// stop our sub-timers
+	bool success = _lowTimer.stop() && _highTimer.stop();
+	
+	if(success && !suppressEvents){
+		// dispatch our stop event
+		HighLowTimerEvent event = HighLowTimerEvent(_name, this, HIGHLOWTIMER_EVENT_STOP);
+		ofNotifyEvent(highLowTimerEvent, event, this);
+	}
+	
+	return success;
+}
+
+void HighLowTimer::highTimerEventCallback(const void *sender, TimerEvent &event){
+	cout << "HighLowTimer highTimer callback: " << event._name << " " << Timer::getTimerEventTypeAsString(event._type) << endl;
+	switch(event._type){
+		case TIMER_EVENT_COMPLETE:{
+			// ALWAYS want to star the low timer after the high timer has finished
+			// TODO: Need some way to package the value up with this
+			HighLowTimerEvent event = HighLowTimerEvent(_highTimer.getName(), this, HIGHLOWTIMER_EVENT_HIGH_COMPLETE);
+			ofNotifyEvent(highLowTimerEvent, event, this);
+			
+			if(_lowTimer.start()){
+				// send our LOW_START event
+				HighLowTimerEvent event = HighLowTimerEvent(_lowTimer.getName(), _lowValue, this, HIGHLOWTIMER_EVENT_LOW_START);
+				ofNotifyEvent(highLowTimerEvent, event, this);
+			}
+			
+			break;
+		}
+		default:
+			// do nothing
+			break;
+	}
+}
+
+void HighLowTimer::lowTimerEventCallback(const void *sender, TimerEvent &event){
+	cout << "HighLowTimer lowTimer callback: " << event._name << " " << Timer::getTimerEventTypeAsString(event._type) << endl;
+	switch(event._type){
+		case TIMER_EVENT_COMPLETE:{
+			HighLowTimerEvent event = HighLowTimerEvent(_lowTimer.getName(), this, HIGHLOWTIMER_EVENT_LOW_COMPLETE);
+			ofNotifyEvent(highLowTimerEvent, event, this);	
+			
+			// have done a cycle
+			_cycleCount++;
+			if(_cycleCount < _cycleLimit || _mode == HIGHLOWTIMER_MODE_REPEAT){
+				// we have more cycles to do
+				if(_highTimer.start()){
+					// notify high timer started
+					HighLowTimerEvent event = HighLowTimerEvent(_highTimer.getName(), _highValue, this, HIGHLOWTIMER_EVENT_HIGH_START);
+					ofNotifyEvent(highLowTimerEvent, event, this);					
+				}
+			}else{
+				// no more cycles, so send the complete event
+				HighLowTimerEvent event = HighLowTimerEvent(_name, this, HIGHLOWTIMER_EVENT_COMPLETE);
+				ofNotifyEvent(highLowTimerEvent, event, this);
+				stop(true); // stop anything that might have been running (safey), suppress any events
+			}
+			break;
+		}
+		default:
+			// do nothing
+			break;
+	}
+}
+
+bool HighLowTimer::isTimerRunning(){
+	return (_highTimer.isTimerRunning() || _lowTimer.isTimerRunning());
+}
+
+string HighLowTimer::getName(){
+	return _name;
+}
+
+void HighLowTimer::setName(string name){
+	_name = name;
+}
+
---
+++ src/HighLowTimer.h
@@ -0,0 +1,196 @@
+/*
+ *  HighLowTimer.h
+ *
+ */
+
+
+#ifndef	_HIGHLOWTIMER_H
+#define _HIGHLOWTIMER_H
+
+#include "ofMain.h"
+#include "ofThread.h"
+#include "ofEvents.h"
+#include "Timer.h"
+
+/*
+ 
+ TODO: This documentation is slightly out of date, especially regarding events
+ 
+ Timer that will alternate between two values for a set number of cycles (or infinitely)
+
+ The timer begins with "high".
+ 
+ Events are triggered when timers begin, so the first event (with the high value)
+ is sent as soon as you hit start(). Once the high timer is finished, the "value"
+ changes to Low, the HighLowValueChanged is triggered with the low value, and the
+ low timer is started.
+
+ There are two kinds of events:
+	-	HighLowValueChanged, before each timer is about to run,
+		with the corresponding high or low value included.
+	-	HighLowTimerFinished, fired when the requested number of cycles is complete
+ 
+ Usage:
+ 
+ HighLowTimer hlt()
+ 
+ // High timer for 4 seconds, high value of 1.0, low timer for 2 seconds, low value of 0.0
+ // repeat infinitely, timer resolution of 51 milliseconds
+ hlt.setup(4000, 1.0, 2000, 0.0)
+ // same as hlt.setup(4000, 1.0, 2000, 0.0, HighLowTimer::INFINITE, 51_)
+ ofAddListener(hlt.HighLowValueChanged, this, MyClass::MyValueChangedMethod);
+ ofAddListener(hlt.HighLowTimerFinished, this, MyClass::HighLowTimerHasFinishedMethod);
+ hlt.start();
+
+ */
+
+enum HighLowTimerEventType{
+	HIGHLOWTIMER_EVENT_START = 0,
+	HIGHLOWTIMER_EVENT_COMPLETE,
+	HIGHLOWTIMER_EVENT_RESTART,
+	HIGHLOWTIMER_EVENT_STOP,
+	HIGHLOWTIMER_EVENT_HIGH_START,
+	HIGHLOWTIMER_EVENT_HIGH_COMPLETE,
+	HIGHLOWTIMER_EVENT_LOW_START,
+	HIGHLOWTIMER_EVENT_LOW_COMPLETE,
+	HIGHLOWTIMER_EVENT_FAIL,
+    HIGHLOWTIMER_EVENT_UNKNOWN
+};
+
+enum HighLowTimerMode{
+	HIGHLOWTIMER_MODE_CYCLECOUNT = 0,
+    HIGHLOWTIMER_MODE_REPEAT,
+    HIGHLOWTIMER_MODE_UNKNOWN
+};
+
+class HighLowTimerEvent;
+class HighLowTimer {
+	
+public:
+	
+	static const int REPEAT = -1;
+	
+	static inline string getHighLowTimerModeAsString(HighLowTimerMode mode){
+        switch (mode) {
+            case HIGHLOWTIMER_MODE_CYCLECOUNT:
+                return "HIGHLOWTIMER_MODE_CYCLECOUNT";
+                break;
+            case HIGHLOWTIMER_MODE_REPEAT:
+                return "HIGHLOWTIMER_MODE_REPEAT";
+                break; 
+            default:
+                return "HIGHLOWTIMER_MODE_UNKNOWN";
+                break;
+        }
+    };
+    
+    static inline string getHighLowTimerEventTypeAsString(HighLowTimerEventType type){
+        switch (type) {
+			case HIGHLOWTIMER_EVENT_START:
+				return "HIGHLOWTIMER_EVENT_START";
+				break;
+			case HIGHLOWTIMER_EVENT_COMPLETE:
+				return "HIGHLOWTIMER_EVENT_COMPLETE";
+				break;
+			case HIGHLOWTIMER_EVENT_RESTART:
+				return "HIGHLOWTIMER_EVENT_RESTART";
+				break;
+			case HIGHLOWTIMER_EVENT_STOP:
+				return "HIGHLOWTIMER_EVENT_STOP";
+				break;
+			case HIGHLOWTIMER_EVENT_HIGH_START:
+				return "HIGHLOWTIMER_EVENT_HIGH_START";
+				break;
+			case HIGHLOWTIMER_EVENT_HIGH_COMPLETE:
+				return "HIGHLOWTIMER_EVENT_HIGH_COMPLETE";
+				break;
+			case HIGHLOWTIMER_EVENT_LOW_START:
+				return "HIGHLOWTIMER_EVENT_LOW_START";
+				break;
+			case HIGHLOWTIMER_EVENT_LOW_COMPLETE:
+				return "HIGHLOWTIMER_EVENT_LOW_COMPLETE";
+				break;
+			case HIGHLOWTIMER_EVENT_FAIL:
+				return "HIGHLOWTIMER_EVENT_FAIL";
+				break;
+			case HIGHLOWTIMER_EVENT_UNKNOWN:
+				return "HIGHLOWTIMER_EVENT_UNKNOWN";
+				break;
+            default:
+                return "HIGHLOWTIMER_EVENT_UNKNOWN";
+                break;
+        }
+    };
+	
+	HighLowTimer();
+	~HighLowTimer();
+	
+	void setup(int highLengthInMilliseconds, float highValue,
+			   int lowLengthInMilliseconds, float lowValue,
+			   HighLowTimerMode mode = HIGHLOWTIMER_MODE_REPEAT,
+			   string timerName = "UnnamedHighLowTimer",
+			   int timerResolution = 51);
+
+	void setup(int highLengthInMilliseconds, float highValue,
+			   int lowLengthInMilliseconds, float lowValue,
+			   int cycleLimit = 1,
+			   string timerName = "UnnamedHighLowTimer",
+			   int timerResolution = 51);	
+   	
+	bool start();
+	bool restart();
+	bool stop();
+
+	void highTimerEventCallback(const void *sender, TimerEvent &event);
+	void lowTimerEventCallback(const void *sender, TimerEvent &event);
+	
+	bool isTimerRunning();
+	string getName();
+	void setName(string name);
+	
+	ofEvent<HighLowTimerEvent> highLowTimerEvent;
+	
+private:
+	
+	bool start(bool suppressEvents);
+	bool stop(bool suppressEvents);
+	
+	Timer	_highTimer;
+	Timer	_lowTimer;
+	
+	int	_highLength;
+	int _lowLength;
+	float _highValue;
+	float _lowValue;
+	int _cycleLimit;
+	int _cycleCount;	
+	
+	HighLowTimerMode _mode;
+	
+	int _timerResolution;
+	
+	string _name;
+};
+
+class HighLowTimerEvent{
+    
+public:
+
+	HighLowTimerEvent(string name, HighLowTimer * timer, HighLowTimerEventType type):
+    _name(name), _value(0.0), _timer(timer), _type(type){};
+
+    HighLowTimerEvent(string name, float value, HighLowTimer * timer, HighLowTimerEventType type):
+    _name(name), _value(value), _timer(timer), _type(type){};
+    
+    string _name;
+	float _value;
+    HighLowTimer * _timer;
+    HighLowTimerEventType _type;
+};
+
+
+
+#endif
+
+
+
